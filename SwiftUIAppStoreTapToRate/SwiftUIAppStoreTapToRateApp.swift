f1b046b8280b8df2ee0c4961e028b9de8b76571d
sbiljeek <sbiljeekdev@gmail.com>
2023-04-10 11:35:41 +0300

    Initial Commit

---
+++ SwiftUIAppStoreTapToRate/SwiftUIAppStoreTapToRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppStoreTapToRateApp.swift
+//  SwiftUIAppStoreTapToRate
+//
+//  Created by Salman Biljeek on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppStoreTapToRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
