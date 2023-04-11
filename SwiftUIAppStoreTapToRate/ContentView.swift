3af2f649939895fa75afd7c530261a40168743f8
sbiljeek <sbiljeekdev@gmail.com>
2023-04-11 09:59:14 +0300

    initial commit

--- SwiftUIAppStoreTapToRate/ContentView.swift
+++ SwiftUIAppStoreTapToRate/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedStars: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("Tap to Rate:")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 17, weight: .regular))
+                Spacer()
+                HStack(spacing: 17) {
+                    ForEach(Array((1...5).enumerated()), id: \.offset) { index, element in
+                        Button {
+                            handleStarTapped(star: index + 1)
+                        } label: {
+                            Image(systemName: (self.selectedStars >= (index + 1)) ? "star.fill" : "star")
+                                .resizable()
+                                .frame(maxWidth: 27, maxHeight: 27)
+                                .foregroundColor(.blue)
+                        }
+                    }
+                }
+            }
+            Spacer()
+            resetButtonView
+            Spacer()
         }
-        .padding()
+        .padding(.top, 40)
+        .padding([.leading, .trailing], 20)
+    }
+    
+    func handleStarTapped(star: Int) {
+        self.selectedStars = star
+    }
+    
+    var resetButtonView: some View {
+        Button {
+            self.selectedStars = 0
+        } label: {
+            Text("Reset")
+                .padding()
+                .padding([.leading, .trailing])
+                .foregroundColor(.white)
+                .background(.indigo)
+                .clipShape(Capsule())
+                .font(.system(size: 18, weight: .bold))
+        }
     }
 }
 
